e3174967b748e3870b65a463050c8f56c852a4b3
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2021-01-04 22:54:58 -0500

    added some components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import Router from "react-router-dom";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Wrapper from "./components/Wrapper/index";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/pages/about";
+import Portfolio from "./components/pages/portfolio";
 
 function App() {
   return (
@@ -10,12 +13,15 @@ function App() {
       <div>
         <NavBar />
         <Header />
-        <Router exact path ="/" component={Home} />
-        <Router exact path ="/portfolio" component={Portfolio} />
+        <Wrapper>
+          <Route exact path ="/" component={About} />
+          <Route path ="/about" component={About} />
+          <Route path ="/portfolio" component={Portfolio} />
+        </Wrapper>
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
